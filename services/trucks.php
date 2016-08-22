1e878b618bd6158f8de6aaff06c5fce1a06c515e
Alejandro Avella <alejandroavella77@yahoo.com>
2016-08-21 19:58:45 -0700

    Add files via upload

---
+++ services/trucks.php
@@ -0,0 +1,20 @@
+<?php
+	
+include 'config.php';
+
+$sql = "select * from trucks2";
+		
+try {
+	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$stmt = $dbh->prepare($sql);  
+	$stmt->bindParam("id", $_GET[id]);
+	$stmt->execute();
+	$employees = $stmt->fetchAll(PDO::FETCH_OBJ);
+	$dbh = null;
+	echo '{"items":'. json_encode($employees) .'}'; 
+} catch(PDOException $e) {
+	echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+}
+
+?>
